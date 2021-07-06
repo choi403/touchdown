2420c13433cedffbd7d04d7986ed9fa77a2fcd7a
June Choi <wchoi403@gmail.com>
2021-07-06 22:28:05 +0900

    Greedy baseline implementation (wip)

--- sdr/clip_beam_search.py
+++ sdr/clip_beam_search.py
@@ -43,6 +43,136 @@ image_std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).cuda()
 # ! pip install ftfy regex
 # ! wget https://openaipublic.azureedge.net/clip/bpe_simple_vocab_16e6.txt.gz -O bpe_simple_vocab_16e6.txt.gz
 
+#@title
+
+import gzip
+import html
+import os
+from functools import lru_cache
+
+import ftfy
+import regex as re
+
+
+@lru_cache()
+def bytes_to_unicode():
+    """
+    Returns list of utf-8 byte and a corresponding list of unicode strings.
+    The reversible bpe codes work on unicode strings.
+    This means you need a large # of unicode characters in your vocab if you want to avoid UNKs.
+    When you're at something like a 10B token dataset you end up needing around 5K for decent coverage.
+    This is a signficant percentage of your normal, say, 32K bpe vocab.
+    To avoid that, we want lookup tables between utf-8 bytes and unicode strings.
+    And avoids mapping to whitespace/control characters the bpe code barfs on.
+    """
+    bs = list(range(ord("!"), ord("~")+1))+list(range(ord("¡"), ord("¬")+1))+list(range(ord("®"), ord("ÿ")+1))
+    cs = bs[:]
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            bs.append(b)
+            cs.append(2**8+n)
+            n += 1
+    cs = [chr(n) for n in cs]
+    return dict(zip(bs, cs))
+
+
+def get_pairs(word):
+    """Return set of symbol pairs in a word.
+    Word is represented as tuple of symbols (symbols being variable-length strings).
+    """
+    pairs = set()
+    prev_char = word[0]
+    for char in word[1:]:
+        pairs.add((prev_char, char))
+        prev_char = char
+    return pairs
+
+
+def basic_clean(text):
+    text = ftfy.fix_text(text)
+    text = html.unescape(html.unescape(text))
+    return text.strip()
+
+
+def whitespace_clean(text):
+    text = re.sub(r'\s+', ' ', text)
+    text = text.strip()
+    return text
+
+
+class SimpleTokenizer(object):
+    def __init__(self, bpe_path: str = "bpe_simple_vocab_16e6.txt.gz"):
+        self.byte_encoder = bytes_to_unicode()
+        self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
+        merges = gzip.open(bpe_path).read().decode("utf-8").split('\n')
+        merges = merges[1:49152-256-2+1]
+        merges = [tuple(merge.split()) for merge in merges]
+        vocab = list(bytes_to_unicode().values())
+        vocab = vocab + [v+'</w>' for v in vocab]
+        for merge in merges:
+            vocab.append(''.join(merge))
+        vocab.extend(['<|startoftext|>', '<|endoftext|>'])
+        self.encoder = dict(zip(vocab, range(len(vocab))))
+        self.decoder = {v: k for k, v in self.encoder.items()}
+        self.bpe_ranks = dict(zip(merges, range(len(merges))))
+        self.cache = {'<|startoftext|>': '<|startoftext|>', '<|endoftext|>': '<|endoftext|>'}
+        self.pat = re.compile(r"""<\|startoftext\|>|<\|endoftext\|>|'s|'t|'re|'ve|'m|'ll|'d|[\p{L}]+|[\p{N}]|[^\s\p{L}\p{N}]+""", re.IGNORECASE)
+
+    def bpe(self, token):
+        if token in self.cache:
+            return self.cache[token]
+        word = tuple(token[:-1]) + ( token[-1] + '</w>',)
+        pairs = get_pairs(word)
+
+        if not pairs:
+            return token+'</w>'
+
+        while True:
+            bigram = min(pairs, key = lambda pair: self.bpe_ranks.get(pair, float('inf')))
+            if bigram not in self.bpe_ranks:
+                break
+            first, second = bigram
+            new_word = []
+            i = 0
+            while i < len(word):
+                try:
+                    j = word.index(first, i)
+                    new_word.extend(word[i:j])
+                    i = j
+                except:
+                    new_word.extend(word[i:])
+                    break
+
+                if word[i] == first and i < len(word)-1 and word[i+1] == second:
+                    new_word.append(first+second)
+                    i += 2
+                else:
+                    new_word.append(word[i])
+                    i += 1
+            new_word = tuple(new_word)
+            word = new_word
+            if len(word) == 1:
+                break
+            else:
+                pairs = get_pairs(word)
+        word = ' '.join(word)
+        self.cache[token] = word
+        return word
+
+    def encode(self, text):
+        bpe_tokens = []
+        text = whitespace_clean(basic_clean(text)).lower()
+        for token in re.findall(self.pat, text):
+            token = ''.join(self.byte_encoder[b] for b in token.encode('utf-8'))
+            bpe_tokens.extend(self.encoder[bpe_token] for bpe_token in self.bpe(token).split(' '))
+        return bpe_tokens
+
+    def decode(self, tokens):
+        text = ''.join([self.decoder[token] for token in tokens])
+        text = bytearray([self.byte_decoder[c] for c in text]).decode('utf-8', errors="replace").replace('</w>', ' ')
+        return text
+
 import os
 import skimage
 import IPython.display
@@ -222,6 +352,74 @@ def crop(input, row, col):
     
     return cropped_images
     
-    
+# ! Greedy baseline search algorithm 
 # ! 1. Slice image into m1 * n1 pieces
-# ! 2. 
+# ! 2. Get similarity scores using the first phrase
+# ! 3. Slice image into m2 * n2 pieces
+# ! 4. Get similarity scores using the second phrase
+# ! 5. ... 
+# ! 6. (optional, for the last phrase) Switch the word "touchdown" and edit something into the image
+# ! 7. Slice image into mi * ni pieces
+# ! 8. Get similarity scores using the last phrase
+
+
+# ! ? 
+image_mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).cuda()
+image_std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).cuda()
+
+def get_image_features(cropped_images, model): 
+    row = len(cropped_images)
+    col = len(cropped_images[0])
+    
+    images = []
+    
+    # Preprocess 
+    for i in range(row):
+        for j in range(col):
+            image = preprocess(cropped_images[i][j])
+            images.append(image)
+            
+    # Stack and normalize
+    image_input = torch.tensor(np.stack(images)).cuda()
+    image_input -= image_mean[:, None, None]
+    image_input /= image_std[:, None, None]
+    
+    # Encode images
+    with torch.no_grad():
+        image_features = model.encode_image(image_input).float()
+    
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    
+    return image_features
+
+def get_text_features(texts, model, tokenizer):
+    text_tokens = [tokenizer.encode(desc) for desc in texts]
+    
+    text_input = torch.zeros(len(text_tokens), model.context_length, dtype=torch.long)
+    sot_token = tokenizer.encoder['<|startoftext|>']
+    eot_token = tokenizer.encoder['<|endoftext|>']
+
+    for i, tokens in enumerate(text_tokens):
+        tokens = [sot_token] + tokens + [eot_token]
+        text_input[i, :len(tokens)] = torch.tensor(tokens)
+
+    text_input = text_input.cuda()
+
+    with torch.no_grad():
+        text_features = model.encode_text(text_input).float()
+    
+    return text_features
+
+
+# ! Test implementation
+def greedy_search(image, texts, model, tokenizer):
+    cropped_1 = crop(image, 3, 3)
+    
+    image_features_1 = get_image_features(cropped_1, model)
+    text_features_1 = get_text_features(texts[0], model, tokenizer)
+    
+    similarity_1 = text_features_1.cpu().numpy() @ image_features_1.cpu().numpy().T
+    
+    print(similarity_1)
+
+    
